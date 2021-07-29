6bf86d270870fcc1f61cb7d8077e663b2dbb54f5
alexjout <alexandre.joutard@student.ecp.fr>
2021-07-29 11:09:21 +0200

    modif to sentence repo base

--- examples/unsupervised_learning/SimCSE/train_askubuntu_simcse.py
+++ examples/unsupervised_learning/SimCSE/train_askubuntu_simcse.py
@@ -7,30 +7,34 @@ import gzip
 from torch.utils.data import DataLoader
 from datetime import datetime
 
+from BertBase.SentenceTransfo.wandb_callback import CustomStepCallback
 
-#### Just some code to print debug information to stdout
+
+# Just some code to print debug information to stdout
 logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO,
                     handlers=[LoggingHandler()])
-#### /print debug information to stdout
+# /print debug information to stdout
 
 # Some training parameters. For the example, we use a batch_size of 128, a max sentence length (max_seq_length)
 # of 32 word pieces and as model roberta-base
 model_name = 'roberta-base'
 batch_size = 128
 max_seq_length = 32
-num_epochs = 1
+num_epochs = 2
 
 ################# Download AskUbuntu and extract training corpus  #################
-askubuntu_folder = 'data/askubuntu'
-output_path = 'output/askubuntu-simcse-{}-{}-{}'.format(model_name, batch_size, datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+askubuntu_folder = '/data0/research_ir/data/others/askubuntu'
+output_path = '/data0/research_ir/data/others/askubuntu/askubuntu-simcse-{}-{}-{}'.format(
+    model_name, batch_size, datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
 
-## Download the AskUbuntu dataset from https://github.com/taolei87/askubuntu
+# Download the AskUbuntu dataset from https://github.com/taolei87/askubuntu
 for filename in ['text_tokenized.txt.gz', 'dev.txt', 'test.txt', 'train_random.txt']:
     filepath = os.path.join(askubuntu_folder, filename)
     if not os.path.exists(filepath):
-        util.http_get('https://github.com/taolei87/askubuntu/raw/master/'+filename, filepath)
+        util.http_get(
+            'https://github.com/taolei87/askubuntu/raw/master/'+filename, filepath)
 
 # Read the corpus
 corpus = {}
@@ -43,12 +47,14 @@ with gzip.open(os.path.join(askubuntu_folder, 'text_tokenized.txt.gz'), 'rt', en
         corpus[id] = title
 
 # Read dev & test dataset
+
+
 def read_eval_dataset(filepath):
     dataset = []
     with open(filepath) as fIn:
         for line in fIn:
             query_id, relevant_id, candidate_ids, bm25_scores = line.strip().split("\t")
-            if len(relevant_id) == 0:   #Skip examples without relevant entries
+            if len(relevant_id) == 0:  # Skip examples without relevant entries
                 continue
 
             relevant_id = relevant_id.split(" ")
@@ -63,12 +69,13 @@ def read_eval_dataset(filepath):
             dev_test_ids.update(candidate_ids)
     return dataset
 
+
 dev_dataset = read_eval_dataset(os.path.join(askubuntu_folder, 'dev.txt'))
 test_dataset = read_eval_dataset(os.path.join(askubuntu_folder, 'test.txt'))
 
 
-## Now we need a list of train sentences.
-## In this example we simply use all sentences that don't appear in the train/dev set
+# Now we need a list of train sentences.
+# In this example we simply use all sentences that don't appear in the train/dev set
 train_sentences = []
 for id, sentence in corpus.items():
     if id not in dev_test_ids:
@@ -79,29 +86,42 @@ logging.info("{} train sentences".format(len(train_sentences)))
 ################# Intialize an SBERT model #################
 
 
-word_embedding_model = models.Transformer(model_name, max_seq_length=max_seq_length)
+word_embedding_model = models.Transformer(
+    model_name, max_seq_length=max_seq_length)
 
 # Apply mean pooling
 pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
                                pooling_mode_mean_tokens=True,
                                pooling_mode_cls_token=False,
                                pooling_mode_max_tokens=False)
-model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+model = SentenceTransformer(
+    modules=[word_embedding_model, pooling_model], device="cuda:1")
 
 ################# Train the model #################
 
 # As Loss function, we use MultipleNegativesRankingLoss
-train_dataloader = DataLoader(train_sentences, batch_size=batch_size, shuffle=True, drop_last=True)
+train_dataloader = DataLoader(
+    train_sentences, batch_size=batch_size, shuffle=True, drop_last=True)
 train_loss = losses.MultipleNegativesRankingLoss(model)
 
 # Create a dev evaluator
-dev_evaluator = evaluation.RerankingEvaluator(dev_dataset, name='AskUbuntu dev')
-test_evaluator = evaluation.RerankingEvaluator(test_dataset, name='AskUbuntu test')
+dev_evaluator = evaluation.RerankingEvaluator(
+    dev_dataset, name='AskUbuntu dev')
+test_evaluator = evaluation.RerankingEvaluator(
+    test_dataset, name='AskUbuntu test')
 
 logging.info("Dev performance before training")
 dev_evaluator(model)
 
 warmup_steps = int(num_epochs*len(train_dataloader)*0.1)
+
+
+class Args:
+    batch_size = batch_size
+
+
+step_callback = CustomStepCallback(
+    Args(), "askubuntu", output_path, log_every_step=20)
 
 logging.info("Start training")
 model.fit(
@@ -112,14 +132,13 @@ model.fit(
     warmup_steps=warmup_steps,
     output_path=output_path,
     show_progress_bar=True,
-    use_amp=True                #If your GPU does not have FP16 cores, set use_amp=False
+    use_amp=True,  # If your GPU does not have FP16 cores, set use_amp=False
+    step_callback=step_callback
 )
 
 latest_output_path = output_path + "-latest"
 model.save(latest_output_path)
 
-### Run test evaluation on the latest model. This is equivalent to not having a dev dataset
+# Run test evaluation on the latest model. This is equivalent to not having a dev dataset
 model = SentenceTransformer(latest_output_path)
 test_evaluator(model)
-
-
--- sentence_transformers/models/Pooling.py
+++ sentence_transformers/models/Pooling.py
@@ -19,6 +19,7 @@ class Pooling(nn.Module):
     :param pooling_mode_mean_tokens: Perform mean-pooling
     :param pooling_mode_mean_sqrt_len_tokens: Perform mean-pooling, but devide by sqrt(input_length).
     """
+
     def __init__(self,
                  word_embedding_dimension: int,
                  pooling_mode: str = None,
@@ -26,12 +27,14 @@ class Pooling(nn.Module):
                  pooling_mode_max_tokens: bool = False,
                  pooling_mode_mean_tokens: bool = True,
                  pooling_mode_mean_sqrt_len_tokens: bool = False,
+                 layer_to_pool_from: list = [-1]
                  ):
         super(Pooling, self).__init__()
 
-        self.config_keys = ['word_embedding_dimension',  'pooling_mode_cls_token', 'pooling_mode_mean_tokens', 'pooling_mode_max_tokens', 'pooling_mode_mean_sqrt_len_tokens']
+        self.config_keys = ['word_embedding_dimension',  'pooling_mode_cls_token',
+                            'pooling_mode_mean_tokens', 'pooling_mode_max_tokens', 'pooling_mode_mean_sqrt_len_tokens']
 
-        if pooling_mode is not None:        #Set pooling mode by string
+        if pooling_mode is not None:  # Set pooling mode by string
             pooling_mode = pooling_mode.lower()
             assert pooling_mode in ['mean', 'max', 'cls']
             pooling_mode_cls_token = (pooling_mode == 'cls')
@@ -44,9 +47,13 @@ class Pooling(nn.Module):
         self.pooling_mode_max_tokens = pooling_mode_max_tokens
         self.pooling_mode_mean_sqrt_len_tokens = pooling_mode_mean_sqrt_len_tokens
 
-        pooling_mode_multiplier = sum([pooling_mode_cls_token, pooling_mode_max_tokens, pooling_mode_mean_tokens, pooling_mode_mean_sqrt_len_tokens])
-        self.pooling_output_dimension = (pooling_mode_multiplier * word_embedding_dimension)
+        pooling_mode_multiplier = sum([pooling_mode_cls_token, pooling_mode_max_tokens,
+                                      pooling_mode_mean_tokens, pooling_mode_mean_sqrt_len_tokens])
+        self.pooling_output_dimension = (
+            pooling_mode_multiplier * word_embedding_dimension)
 
+        self.layer_to_pool_from = layer_to_pool_from
+        self.need_hidden_states = (layer_to_pool_from != [-1])
 
     def __repr__(self):
         return "Pooling({})".format(self.get_config_dict())
@@ -72,31 +79,53 @@ class Pooling(nn.Module):
         cls_token = features['cls_token_embeddings']
         attention_mask = features['attention_mask']
 
-        ## Pooling strategy
+        assert (not self.need_hidden_states) or (
+            "all_layer_embeddings" in features)
+
+        # Pooling strategy
         output_vectors = []
         if self.pooling_mode_cls_token:
             output_vectors.append(cls_token)
         if self.pooling_mode_max_tokens:
-            input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-            token_embeddings[input_mask_expanded == 0] = -1e9  # Set padding tokens to large negative value
+            input_mask_expanded = attention_mask.unsqueeze(
+                -1).expand(token_embeddings.size()).float()
+            # Set padding tokens to large negative value
+            token_embeddings[input_mask_expanded == 0] = -1e9
             max_over_time = torch.max(token_embeddings, 1)[0]
             output_vectors.append(max_over_time)
         if self.pooling_mode_mean_tokens or self.pooling_mode_mean_sqrt_len_tokens:
-            input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-            sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+            if not self.need_hidden_states:
+                input_mask_expanded = attention_mask.unsqueeze(
+                    -1).expand(token_embeddings.size()).float()
+                sum_embeddings = torch.sum(
+                    token_embeddings * input_mask_expanded, 1)
 
-            #If tokens are weighted (by WordWeights layer), feature 'token_weights_sum' will be present
-            if 'token_weights_sum' in features:
-                sum_mask = features['token_weights_sum'].unsqueeze(-1).expand(sum_embeddings.size())
+                # If tokens are weighted (by WordWeights layer), feature 'token_weights_sum' will be present
+                if 'token_weights_sum' in features:
+                    sum_mask = features['token_weights_sum'].unsqueeze(
+                        -1).expand(sum_embeddings.size())
+                else:
+                    sum_mask = input_mask_expanded.sum(1)
+
             else:
+                hidden_states = features["all_layer_embeddings"]
+                hidden_states = torch.stack(
+                    [hidden_states[idx] for idx in self.layer_to_pool_from])  # nb_hidden_states x bs x sentence_length x emb_dim
+
+                input_mask_expanded = attention_mask.unsqueeze(
+                    -1).expand(token_embeddings.size()).float()
+
+                sum_embeddings = torch.sum(
+                    hidden_states * input_mask_expanded, (0, 2))  # bs x emb_dim
+                # tokens weighted not handled for the moment
                 sum_mask = input_mask_expanded.sum(1)
 
             sum_mask = torch.clamp(sum_mask, min=1e-9)
-
             if self.pooling_mode_mean_tokens:
                 output_vectors.append(sum_embeddings / sum_mask)
             if self.pooling_mode_mean_sqrt_len_tokens:
-                output_vectors.append(sum_embeddings / torch.sqrt(sum_mask))
+                output_vectors.append(
+                    sum_embeddings / torch.sqrt(sum_mask))
 
         output_vector = torch.cat(output_vectors, 1)
         features.update({'sentence_embedding': output_vector})
